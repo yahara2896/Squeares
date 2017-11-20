75f44e677fe5284a2bb4cdd66ebfbd4c00267f19
50616046 <50616046@50616046-pc>
2017-11-20 21:19:21 +0900

    理論値:y=0.7619x+4.1286 計算値:y=0.761906x+4.128568

---
+++ src/squares/Squares_main.java
@@ -0,0 +1,25 @@
+package squares;
+
+public class Squares_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double x[] = {0.0,0.3,0.6,0.9,1.2,1.5,1.8};
+		double y[] = { 4.0, 4.9, 4.2,5.0, 4.6, 5.2,5.8 };
+		double theta[] = new double[2];
+
+		Squares_lib slib = new Squares_lib(x, y);
+
+		for(int i = 0; i < 100; i++){
+			theta = slib.getDx();
+			System.out.println(i+" : theta[0] = "+theta[0]);
+			System.out.println(i+" : theta[1] = "+theta[1]);
+			System.out.println(i+" : Objective function = "+slib.getObject());
+			
+		}
+
+		
+	}
+
+}
